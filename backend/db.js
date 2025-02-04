1de38989d5c560a99cd8d8fe6b2235e43b8b73a8
ahmedsallam2 <ahmed234sallam@gmail.com>
2025-02-04 23:26:52 +0200

    Initial commit with Docker and Kubernetes files

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,28 @@
 const mysql = require('mysql2');
 
-// Create a connection pool to the database
 const pool = mysql.createPool({
-  host: 'db',       // Replace with your database host
-  user: 'root',     // Replace with your database username
-  password: 'rootpassword',     // Replace with your database password
-  database: 'angular', // Replace with your database name
+  host: 'db', // MySQL container service name
+  user: 'root',
+  password: 'rootpassword',
+  database: 'angular',
   waitForConnections: true,
-  connectionLimit: 10, // Limit the number of connections
-  queueLimit: 0       // No limit for the queue
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
+// Retry mechanism
+const connectWithRetry = () => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error connecting to the database:', err.message);
+      setTimeout(connectWithRetry, 5000);  // Retry after 5 seconds
+    } else {
+      console.log('Connected to the database');
+      connection.release();
+    }
+  });
+};
+
+connectWithRetry();
 
 module.exports = pool;
